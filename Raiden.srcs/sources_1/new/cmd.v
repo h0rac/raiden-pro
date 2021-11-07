07844b0c58609faec339fe4644794e4d9c2db456
h0rac <h0rac26@gmail.com>
2021-11-07 18:14:53 +0100

    first commit

---
+++ Raiden.srcs/sources_1/new/cmd.v
@@ -0,0 +1,501 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: X-Force Red
+// Engineer: Grzegorz Wypych (h0rac)
+// 
+// Create Date: 10/22/2019 05:52:18 AM
+// Design Name: 
+// Module Name: cmd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module cmd(
+  input clk,
+  input din,
+  input trigger_in,
+  input rst,
+  input finished,
+  input glitched,
+  input glitch_out,
+  input gpio_in1,
+  input gpio_in2,
+  output reg gpio_out,
+  output wire dout,
+  output reg [1:0] force_state = 2'd2,
+  output reg [31:0] glitch_delay,
+  output reg [31:0] glitch_width,
+  output reg [31:0] glitch_count,
+  output reg [31:0] glitch_gap,
+  output reg [31:0] glitch_max,
+  output reg [31:0] uart_trigger_baud,
+  output reg [7:0] uart_trigger_data,
+  output reg armed,
+  output reg reset_glitcher,
+  output reg vstart,
+  output reg invert_trigger,
+  output reg [31:0] reset_target,
+  output reg [31:0] emmc_trigger_data
+  );
+  
+reg fpga_rst;
+parameter AUTO = 2'd2;
+parameter CMD_RST_GLITCHER = 8'd65;
+parameter CMD_FORCE_GLITCH_OUT_STATE = 8'd66;
+parameter CMD_FLAG_STATUS = 8'd68;
+parameter CMD_GLITCH_DELAY = 8'd69;
+parameter CMD_GLITCH_WIDTH = 8'd70;
+parameter CMD_GLITCH_COUNT = 8'd71;
+parameter CMD_ARM = 8'd72;
+parameter CMD_GLITCH_GAP = 8'd73;
+parameter CMD_RST = 8'd74;
+parameter CMD_VSTART = 8'd75;
+parameter CMD_GLITCH_MAX = 8'd76;
+parameter CMD_BUILDTIME = 8'd77;
+parameter CMD_INVERT = 8'd78;
+parameter CMD_RESET_TARGET = 8'd79;
+parameter CMD_GPIO_OUT = 8'd80;
+parameter CMD_UART_TRIGGER = 8'd81;
+parameter CMD_UART_TRIGGER_BAUD = 8'd82;
+parameter CMD_EMMC_TRIGGER_DATA = 8'd83;
+  
+parameter IDLE = 8'd1;
+parameter ACK_4BYTE_CMD = 8'd2;
+parameter GLITCH_WIDTH = 8'd3;
+parameter GLITCH_DELAY_LENGTH = 8'd4;
+parameter GLITCH_COUNT = 8'd5;
+parameter ARM = 8'd6;
+parameter GLITCH_GAP = 8'd7;
+parameter FORCE_GLITCH_OUT_STATE = 8'd8;
+parameter RST_GLITCHER = 8'd9;
+parameter GPIO_OUT = 8'd10;
+parameter RST = 8'd11;
+parameter VSTART = 8'd12;
+parameter GLITCH_MAX = 8'd13;
+parameter BUILDTIME = 8'd14;
+parameter FLAG_STATUS = 8'd15;
+parameter INVERT = 8'd16;
+parameter RESET_TARGET = 8'd17;
+parameter UART_TRIGGER = 8'd18;
+parameter UART_TRIGGER_BAUD = 8'd19;
+parameter EMMC_TRIGGER_DATA = 8'd20;
+  
+reg [4:0] state = IDLE;
+wire bit_out;
+wire [7:0] rx_data;
+wire rx_valid;
+
+parameter BAUD = 115200;
+  
+uart_rx rxi (
+  .clk(clk),
+  .rst(rst),
+  .din(din),
+  .baud(BAUD),
+  .data_out(rx_data),
+  .valid(rx_valid)
+  );
+  
+reg  tx_en = 1'b0;
+reg [7:0] tx_data;
+wire tx_rdy;
+wire [8:0] flags; 
+  
+assign flags[0]= armed;       // API armed
+assign flags[1]= glitched;    // glitching has started
+assign flags[2]= finished;    // glitching has completed
+assign flags[3]= glitch_out;  // current state of glitch out
+assign flags[4]= trigger_in;  // current state of trigger in
+assign flags[5] = gpio_in1; // GPIO_IN1 status
+assign flags[6] = gpio_out; // GPIO_OUT status
+assign flags[7] = gpio_in2; // GPIO_IN2 status
+  
+uart_tx txi (
+  .clk(clk),
+  .dout(dout),
+  .data_in(tx_data),
+  .en(tx_en),
+  .rdy(tx_rdy)
+  );
+  
+reg u32_rec_enable = 1'd0;
+wire u32_rec_valid;
+wire [31:0] u32_rec_data;
+  
+uint32_receiver u32_rec(
+  .clk(clk),
+  .reset(rst),
+  .uart_data(rx_data),
+  .uart_valid(rx_valid),
+  .data(u32_rec_data),
+  .data_valid(u32_rec_valid),
+  .enable(u32_rec_enable)
+);
+
+wire [31:0] buildtime;     
+reg [2:0] count = 3'b0;
+always @(posedge clk)
+  begin
+    if(rst)
+      begin
+        state <= IDLE;
+      end
+    if(reset_glitcher)
+      begin
+        reset_glitcher <= 0;
+      end
+  else
+  begin
+    u32_rec_enable <= 1'b0;
+    glitch_delay <= glitch_delay;
+    glitch_count <= glitch_count;
+    glitch_width <= glitch_width;
+    glitch_max <= glitch_max;
+    glitch_gap <= glitch_gap;
+    uart_trigger_data <= uart_trigger_data;
+    uart_trigger_baud <= uart_trigger_baud;
+    emmc_trigger_data <= emmc_trigger_data;
+    state <= state;
+    if(state == BUILDTIME || state == ACK_4BYTE_CMD)
+      begin
+        tx_en <= 1'b1;
+      end
+    else
+      begin
+        tx_en <= 1'b0;
+      end
+    case(state)
+      IDLE:
+        begin
+          if(rx_valid)
+            begin
+              case(rx_data)
+                CMD_FORCE_GLITCH_OUT_STATE:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1; 
+                    state <= FORCE_GLITCH_OUT_STATE;
+                  end
+                CMD_RST_GLITCHER:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1; 
+                    state <= RST_GLITCHER;
+                  end
+                CMD_RST:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1; 
+                    state <= RST;
+                  end
+                CMD_FLAG_STATUS:
+                  begin
+                    tx_en <= 1'b1;
+                    state <= FLAG_STATUS;
+                  end
+                CMD_GLITCH_DELAY:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= GLITCH_DELAY_LENGTH;
+                  end
+                CMD_GLITCH_WIDTH:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= GLITCH_WIDTH;
+                  end
+                CMD_GLITCH_COUNT:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= GLITCH_COUNT;
+                  end
+                CMD_GLITCH_GAP:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= GLITCH_GAP;
+                  end
+                CMD_GLITCH_MAX:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= GLITCH_MAX;
+                  end
+                CMD_EMMC_TRIGGER_DATA:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= EMMC_TRIGGER_DATA;
+                  end
+                CMD_RESET_TARGET:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;
+                    u32_rec_enable <= 1'b1;
+                    state <= RESET_TARGET;
+                  end
+                CMD_ARM:
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;    
+                    state <= ARM;
+                  end
+                CMD_VSTART: // set glitch line voltage to HIGH or LOW
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;    
+                    state <= VSTART;
+                  end
+                CMD_INVERT: // set trigger to inverted
+                  begin
+                    tx_data <= rx_data;
+                    tx_en <= 1'b1;    
+                    state <= INVERT;
+                  end
+                CMD_GPIO_OUT: // set GPIO_OUT state
+                    begin
+                      tx_data <= rx_data;
+                      tx_en <= 1'b1;    
+                      state <= GPIO_OUT;
+                     end
+                CMD_UART_TRIGGER: // set UART_TRIGGER data state
+                    begin
+                      tx_data <= rx_data;
+                      tx_en <= 1'b1;    
+                      state <= UART_TRIGGER;
+                     end
+                CMD_UART_TRIGGER_BAUD: // set UART_TRIGGER baud rate
+                    begin
+                        tx_data <= rx_data;
+                        tx_en <= 1'b1;
+                        u32_rec_enable <= 1'b1;
+                        state <= UART_TRIGGER_BAUD;
+                     end
+                CMD_BUILDTIME:
+                  begin
+//                    tx_data <= rx_data;
+                    tx_en <= 1'b1;    
+                    state <= BUILDTIME;
+                  end
+//               default:
+////                  tx_en <= 1'b0;
+              endcase     
+            end
+        end
+     FORCE_GLITCH_OUT_STATE:
+      begin
+        if(rx_valid)
+          begin
+            force_state <= rx_data;
+            tx_data <= rx_data;
+            tx_en <= 1'b1;  
+            state <= IDLE;
+          end
+        end
+     RST_GLITCHER:
+      begin
+        if(rx_valid)
+          begin
+            reset_glitcher <= rx_data != 32'b0;
+            tx_data <= rx_data;
+            tx_en <= 1'b1;
+            state <= IDLE;
+          end
+      end
+     RST:
+     //TODO reset all params and data to defaul raiden state
+      begin
+        if(rx_valid)
+          begin
+            fpga_rst <= rx_data != 32'b0;
+            tx_data <= rx_data;
+            tx_en <= 1'b1;
+          end
+      end
+      GLITCH_DELAY_LENGTH:
+        begin
+          if(u32_rec_valid)
+            begin
+              glitch_delay <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+      RESET_TARGET:
+        begin
+          if(u32_rec_valid)
+            begin
+              reset_target <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+      ACK_4BYTE_CMD:
+        begin
+          if(tx_rdy)
+            begin
+              tx_data <= u32_rec_data[(count *8 ) +:8]; 
+              count <= count +1;
+              if(count == 3)
+                begin
+                  state <= IDLE;
+                  count <= 0;
+               end
+             end   
+         end       
+      GLITCH_WIDTH:
+        begin
+          if(u32_rec_valid)
+            begin
+              glitch_width <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+      GLITCH_COUNT:
+       begin
+        if(u32_rec_valid)
+          begin
+            glitch_count <= u32_rec_data;
+            state <= ACK_4BYTE_CMD;
+            count <= count -1;
+          end
+       end
+      GLITCH_GAP:
+        begin
+          if(u32_rec_valid)
+            begin
+              glitch_gap <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+      GLITCH_MAX:
+        begin
+          if(u32_rec_valid)
+            begin
+              glitch_max <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+     UART_TRIGGER_BAUD:
+        begin
+          if(u32_rec_valid)
+            begin
+              uart_trigger_baud <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+          end
+      EMMC_TRIGGER_DATA:
+        begin
+          if(u32_rec_valid)
+            begin
+              emmc_trigger_data <= u32_rec_data;
+              state <= ACK_4BYTE_CMD;
+              count <= count -1;
+            end
+        end
+      ARM:
+        begin
+         if(rx_valid)
+           begin
+             armed <= rx_data != 8'b0;
+             tx_data <= rx_data;
+             tx_en <= 1'b1;    
+             state <= IDLE;
+           end
+       end
+      UART_TRIGGER:
+        begin
+         if(rx_valid)
+           begin
+             uart_trigger_data <= rx_data;
+             tx_data <= rx_data;
+             tx_en <= 1'b1;    
+             state <= IDLE;
+           end
+       end
+      VSTART:
+        begin
+          if(rx_valid)
+            begin
+              vstart <= rx_data != 8'b0;
+              tx_data <= rx_data;
+              tx_en <= 1'b1;    
+              state <= IDLE;
+            end
+        end
+      INVERT:
+        begin
+          if(rx_valid)
+            begin
+              invert_trigger <= rx_data != 8'b0;
+              tx_data <= rx_data;
+              tx_en <= 1'b1;    
+              state <= IDLE;
+            end
+        end
+      GPIO_OUT:
+        begin
+         if(rx_valid)
+           begin
+            gpio_out <= rx_data != 8'b0;
+            tx_data <= rx_data;
+            tx_en <= 1'b1;    
+            state <= IDLE;
+           end
+         end
+      FLAG_STATUS:
+        begin
+          if(tx_rdy)
+            begin
+             tx_data <= flags;
+             state <= IDLE;
+            end
+        end
+      BUILDTIME:
+        begin
+          if(tx_rdy)
+            begin
+              tx_data <= buildtime[(count *8 ) +:8]; 
+                count <= count +1;
+                if(count == 3)
+                  begin
+                    state <= IDLE;
+                    count <= 0;
+                  end  
+              end
+          end
+    endcase
+   end
+ end
+ 
+`ifndef __ICARUS__
+    USR_ACCESSE2 U_buildtime (
+       .CFGCLK(),
+       .DATA(buildtime),
+       .DATAVALID()
+    );
+ `else
+    assign buildtime = 0;
+ `endif
+ endmodule
---
+++ Raiden.srcs/sources_1/new/top.v
@@ -0,0 +1,259 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: X-Force Red
+// Engineer: Grzegorz Wypych (h0rac)
+// 
+// Create Date: 10/21/2019 01:53:37 AM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module top(
+  input clk,
+  input rst,
+  input emmc_clk,
+  input emmc_dat0,
+  input ftdi_rx,
+  input target_rx,
+  input trigger_in,
+  input gpio_in1,
+  input gpio_in2,
+  output gpio_out,
+  output ftdi_tx,
+  output led0_g,
+  output led0_r,
+  output led0_b,
+  output led1_b,
+  output led2_g,
+  output led2_r,
+  output led2_b,
+  output led3_b,
+  output led_blink,
+  output led5_debug,
+  output led6_debug,
+  output debug_io0_14_p30,
+  output led_glitch_out,
+  output wire glitch_out,
+  output invert_glitch_out,
+  output wire reset_out
+    );
+ 
+//  assign target_tx = ftdi_rx;
+//  assign target_rx = ftdi_tx;
+
+
+parameter AUTO = 2'd2;
+
+wire bit_out;
+wire active;
+
+wire [31:0] glitch_delay;
+wire [31:0] glitch_width;
+wire [31:0] glitch_count;
+wire [31:0] glitch_gap;
+wire [31:0] glitch_max;
+wire [31:0] reset_target;
+wire [31:0] uart_trigger_baud;
+wire [7:0] uart_trigger_data;
+wire [31:0] emmc_trigger_data;
+wire armed;
+wire glitched;
+wire finished;
+wire vstart; // glitch_out startup value
+wire invert_trigger;
+wire [1:0] force_state;
+wire reset_glitcher;
+
+// counter for main loop
+reg [31:0] counter;
+
+wire emmc_trigger;
+
+emmc emmc_inst (
+  .rst(rst || reset_glitcher),
+  .emmc_clk(emmc_clk || clk),
+  .emmc_dat0(emmc_dat0),
+  .emmc_trigger_data(emmc_trigger_data),
+  .emmc_trigger(emmc_trigger)
+);
+
+cmd cmd_inst
+(
+  .clk(clk),
+  .rst(rst),
+  .din(ftdi_rx),
+  .dout(bit_out),
+  .trigger_in(trigger_in),
+  .glitch_delay(glitch_delay),
+  .glitch_width(glitch_width),
+  .glitch_count(glitch_count),
+  .glitch_gap(glitch_gap),
+  .glitch_max(glitch_max),
+  .emmc_trigger_data(emmc_trigger_data),
+  .armed(armed),
+  .finished(finished),
+  .glitched(glitched),
+  .glitch_out(glitch_out),
+  .force_state(force_state),
+  .reset_glitcher(reset_glitcher),
+  .vstart(vstart),
+  .invert_trigger(invert_trigger),
+  .reset_target(reset_target),
+  .gpio_in1(gpio_in1),
+  .gpio_in2(gpio_in2),
+  .gpio_out(gpio_out),
+  .uart_trigger_data(uart_trigger_data),
+  .uart_trigger_baud(uart_trigger_baud)
+  );   
+  assign ftdi_tx =  bit_out;
+  assign gpio_out = gpio_out;
+ 
+
+wire [7:0] rx_data;
+wire rx_valid;
+
+//handle data byte for UART trigger
+uart_rx rxi_uart_trigger (
+  .clk(clk),
+  .rst(rst),
+  .baud(uart_trigger_baud),
+  .din(target_rx),
+  .data_out(rx_data),
+  .valid(rx_valid)
+  );
+
+wire uart_trigger;
+assign uart_trigger = rx_valid && (uart_trigger_data == rx_data) ? 1: 0;
+
+ 
+ // reset target feature
+// assign rst_out = reset_target ? 1'b0 : 1'b1;
+wire glitch;
+wire trigger;
+
+assign trigger = invert_trigger ^ trigger_in ^ uart_trigger ^ emmc_trigger;
+assign glitch_out = force_state != AUTO ? force_state : glitch;
+assign invert_glitch_out = !glitch_out;
+wire enable =  (force_state == AUTO && (((armed && !finished) && trigger) || (glitch_max && glitched && !finished)));
+ 
+ glitch glitchi
+ (
+  .clk(clk),
+  .rst(rst || reset_glitcher),
+  .enable(enable),
+  .vout(glitch),
+  .glitch_delay(glitch_delay),
+  .glitch_width(glitch_width),
+  .glitch_count(glitch_count),
+  .glitch_gap(glitch_gap),
+  .glitch_max(glitch_max),
+  .glitched(glitched),
+  .vstart(vstart),
+  .finished(finished),
+  .reset_target(reset_target),
+  .reset_out(reset_out)
+ );
+ 
+  // LD0 - BLUE armed, GREEN glitching started, RED finished
+  
+   pwm pwm_led0_r (
+    .clk(clk),
+    .duty(64),
+    .signal(finished),
+    .state(led0_r)
+   );
+   
+   pwm pwm_led0_g (
+    .clk(clk),
+    .duty(16),
+    .signal(glitched && !finished),
+    .state(led0_g)
+   );
+   
+   pwm pwm_led0_b (
+    .clk(clk),
+    .duty(64),
+    .signal(!glitched && armed),
+    .state(led0_b)
+   );
+
+    //LD1 - UART trigger
+   
+//   pwm pwm_led1_r (
+//    .clk(clk),
+//    .duty(64),
+//    .signal(armed),
+//    .state(led1_r)
+//   );
+   
+//   pwm pwm_led1_g (
+//    .clk(clk),
+//    .duty(64),
+//    .signal(armed),
+//    .state(led1_g)
+//   );
+             
+   pwm pwm_led1_b (
+    .clk(clk),
+    .duty(16),
+    .signal(uart_trigger),
+    .state(led1_b)
+   );
+   
+   // LD2 - trigger
+          
+   pwm pwm_led2_r (
+    .clk(clk),
+    .duty(32),
+    .signal(trigger_in),
+    .state(led2_r)
+   );
+   
+   pwm pwm_led2_g (
+    .clk(clk),
+    .duty(4),
+    .signal(trigger_in),
+    .state(led2_g)
+   );
+                      
+   pwm pwm_led2_b (
+    .clk(clk),
+    .duty(48),
+    .signal(trigger_in),
+    .state(led2_b)
+   );
+   
+      // LD3 - emmc trigger 
+          
+                  
+   pwm pwm_led3_b (
+    .clk(clk),
+    .duty(48),
+    .signal(emmc_trigger),
+    .state(led3_b)
+   );
+
+  //assign debug_enable = (armed && trigger_in) || (armed && !trigger_in_pullup);
+  assign debug_io0_14_p30 = uart_trigger;
+  assign led5_debug = reset_out;
+  assign led6_debug = reset_target;
+  assign led_blink = counter[26];
+  assign led_glitch_out = glitch_out;
+  
+  always @(posedge clk)
+  begin
+    counter <= counter + 1;
+  end
+ 
+endmodule
